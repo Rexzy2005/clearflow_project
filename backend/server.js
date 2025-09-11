3bea5ab98368c06e2a787cc80069ea4178c3f55b
DevRex <timothypererat2004@gmail.com>
2025-09-12 00:50:57 +0100

    made changes in the backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
