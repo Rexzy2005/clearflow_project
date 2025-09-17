f571ba7da08050dea28fbb8709c675b198e5a982
DevRex <timothypererat2004@gmail.com>
2025-09-17 12:15:58 +0100

    changes

--- frontend/JS/settings.js
+++ frontend/JS/settings.js
@@ -148,8 +148,8 @@ function showPreview(file) {
 // ===== Load user profile =====
 function loadUserProfile() {
   UserOverviewFullname.textContent = `${currentUser.firstname || ""} ${currentUser.lastname || ""}`;
-  // UserOverviewEmail.textContent = currentUser.email || "";
-  UserOverviewEmail.forEach((Ovemail) => (Ovemail.textContent = currentUser.Ovemail || ""));
+  UserOverviewEmail.textContent = currentUser.email || "";
+  //UserOverviewEmail.forEach((Ovemail) => (Ovemail.textContent = currentUser.Ovemail || ""));
   UserOverviewPhone.textContent = currentUser.phoneNumber || "";
   UserOverviewUname.forEach((name) => (name.textContent = currentUser.username || ""));
   profilePic.forEach((pic) => { pic.src = currentUser.profilePicture || DEFAULT_PROFILE; pic.onerror = () => (pic.src = DEFAULT_PROFILE); });
