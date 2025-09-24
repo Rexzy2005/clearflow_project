78bee947a5093e3ff35cb536c2817ecaacbd6b5a
DevRex <timothypererat2004@gmail.com>
2025-09-24 14:35:53 +0100

    changes

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,9 @@ async function sendEmail(to, subject, otp) {
     service: 'gmail',
     auth: {
       user: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
-      pass: process.env.NODE_CODE_SENDING_EMAIL_PASSWORD
+      pass: process.env.NODE_CODE_SENDING_EMAIL_PASSWORD,
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
     }
   });
 
